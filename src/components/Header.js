e9b4df6e943b054bb358ec50f1ed12e9ccf793a1
brennankapollock <brennankapollock@gmail.com>
2021-07-19 11:11:36 -0500

    working portfolio temp

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import "../styles/index.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import content from "../content";
+import Typical from "react-typical";
+import { useEffect, useState } from "react";
+import {Link as ScrollLink} from "react-scroll";
+
+function Header() {
+    const [animated, setAnimated] = useState(false);
+
+    useEffect(() => {
+        setAnimated(true)
+    },[])
+
+
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center"
+      style={{ background: "#091c29" }}
+    >
+      <div className="w-10/12 mx-auto flex flex-col md:flex-row-reverse items-center justify-between">
+      
+      <div className="w-full md:w-2/5">
+        <LazyLoadImage src={content.header.img} effect="blur" />
+      </div>
+      <div className="text-white font-mono font-bold text-center md:text-left">
+        <h2 className={`${animated ? "" : "translate-y-10 opacity-0"} transform transition duration-1000 ease-in-out text-3xl md:text-5xl font-bold"`}>
+          {content.header.text[0]}
+          <br />
+          {content.header.text[1]}
+        </h2>
+        <h1 className={`${animated ? "" : "translate-y-10 opacity-0"} transform transition duration-1000 ease-in-outfont-bold text-2xl text-gray-500`}>
+          {content.header.text[2]}{" "}
+          <Typical
+            steps={content.header.typical}
+            loop={Infinity}
+            className="inline-block"
+          />
+        </h1>
+        <ScrollLink to="stack" smooth="true">
+        <button className=" animate-float bg-indigo-500 px-10 py-3 text-xl uppercase mt-10 rounded-lg font-bold">{content.header.btnText}</button>
+        </ScrollLink>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import content from "../content";
+import "../styles/index.css";
+
+function Nav() {
+  return (
+    <div style={{background: "#091c29"}}>
+      <div className="flex items-center font-mono justify-between w-10/12 mx-auto py-3 text-white">
+      <h1 className="text-3xl font-bold">BP</h1>
+      <div>
+        {content.nav.links.map((link, index) => {
+          return <span key={index} className="text-xl mr-4">{link.text}</span>;
+        })}
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
