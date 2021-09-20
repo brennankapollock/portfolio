475ceeca9c85816afb310c576b65e778a3b37418
brennankapollock <brennankapollock@gmail.com>
2021-09-19 21:49:18 -0500

    Final Touches on Header

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -4,18 +4,9 @@ import "../styles/index.css";
 function Stack() {
   return (
     <div
-      className="min-h-screen font-averio flex flex-col justify-center items-center bg-black"
+      className="min-h-screen font-averio  bg-black text-white"
       id="stack"
-    >
-      <h1 className="text-white text-center text-5xl">Stack</h1>
-      <ul className="flex text-white flex-col text-xl mr-20 mt-12 text-center">
-        <li className="mt-3">JavaScript E6</li>
-        <li className="mt-3">React</li>
-        <li className="mt-3">Express</li>
-        <li className="mt-3">PostgreSQL</li>
-        <li className="mt-3">Node</li>
-      </ul>
-    </div>
+    ></div>
   );
 }
 
