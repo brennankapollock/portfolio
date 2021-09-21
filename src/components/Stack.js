9a86e4d134e6640c269f86a4818a50d7de495f89
brennankapollock <brennankapollock@gmail.com>
2021-09-20 21:43:38 -0500

    Added Carbon To Stack Com

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -2,68 +2,80 @@ import "../styles/index.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import content from "../content";
-import { useEffect, useState } from "react";
+import { Responsive } from "responsive-react";
+
 Aos.init();
 
 function Stack() {
-  // const [windowSize, setWindowSize] = useState();
-
-  // useEffect(() => {
-  //   setWindowSize(document.documentElement.clientWidth);
-  //   console.log(windowSize);
-  // });
-
-  // function StackDisplay() {
-  //   if (windowSize >= 768) {
-  //     return <img src={content.stack.carbon} />;
-  //   } else {
-  //     return <h1>Test</h1>;
-  //   }
-  // }
-
   return (
     <div
       className="min-h-screen font-averio flex justify-center items-center  bg-black text-white"
       id="stack"
     >
-      <div className="">
-        <img src={content.stack.carbon} />
-      </div>
-
-      {/* <div>
-        <div className="font-bold  text-center  pt-40  flex justify-center items-center  ">
-          <h1 className=" text-white px-3 text-4xl  ">Stack</h1>
+      <Responsive displayIn={["Laptop"]}>
+        <div
+          className="lg:w-3/4 "
+          data-aos="fade-up"
+          data-aos-easing="ease-in"
+          data-aos-duration="1000"
+          data-aos-once="true"
+        >
+          <img src={content.stack.carbon} />
         </div>
+      </Responsive>
 
-        <div className="flex justify-evenly text-2xl mt-12  ">
-          <ul
-            className="text-center"
-            data-aos="fade-up"
-            data-aos-easing="ease-in"
-            data-aos-duration="1500"
-            data-aos-delay="0"
-            data-aos-once="true"
-          >
-            <li className="my-8">React</li>
-            <li className="my-8">Express</li>
-            <li className="my-8">Node</li>
-            <li className="my-8">PostgreSQL</li>
-          </ul>
-          <ul
-            className="text-center"
-            data-aos="fade-up"
-            data-aos-easing="ease-in"
-            data-aos-duration="1500"
-            data-aos-delay="0"
-            data-aos-once="true"
-          >
-            <li className="my-8">MongoDB</li>
-            <li className="my-8">Tailwind</li>
-            <li className="my-8">GraphQL</li>
-            <li className="my-8">Next.js</li>
-          </ul>
+      <Responsive displayIn={["Mobile", "Tablet"]}>
+        <div>
+          <div className="font-bold  text-center   flex justify-center items-center  ">
+            <h1 className=" text-white px-3 text-4xl  ">Stack</h1>
+          </div>
+
+          <div className="flex justify-evenly text-2xl mt-4    ">
+            <ul
+              className="text-center ml-20"
+              data-aos="fade-right"
+              data-aos-easing="ease-in"
+              data-aos-duration="1500"
+              data-aos-delay="0"
+              data-aos-once="true"
+            >
+              <li className="my-8 border border-double border-2 border-white p-2 ">
+                React
+              </li>
+              <li className="my-8 border border-double border-2 border-white p-2">
+                Express
+              </li>
+              <li className="my-8 border border-double border-2 border-white p-2">
+                Node
+              </li>
+              <li className="my-8 border border-double border-2 border-white p-2">
+                PostgreSQL
+              </li>
+            </ul>
+            <ul
+              className="text-center mx-20"
+              data-aos="fade-left"
+              data-aos-easing="ease-in"
+              data-aos-duration="1500"
+              data-aos-delay="0"
+              data-aos-once="true"
+            >
+              <li className="my-8 border border-double border-2 border-white p-2">
+                MongoDB
+              </li>
+              <li className="my-8 border border-double border-2 border-white p-2">
+                Tailwind
+              </li>
+              <li className="my-8 border border-double border-2 border-white p-2">
+                GraphQL
+              </li>
+              <li className="my-8 border border-double border-2 border-white p-2">
+                Next.js
+              </li>
+            </ul>
+          </div>
         </div>
-      </div> */}
+      </Responsive>
     </div>
   );
 }
